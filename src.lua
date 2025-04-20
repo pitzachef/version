34bfb1cc638a27a62423037f410483df4850ef9b
pitzachef <johnathansrfve@gmail.com>
2025-04-20 09:52:25 +0800

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,12 +1,12 @@
 local user = "pitzachef"
 local repo = "version"
-local version = game:HttpGet(string.format("https://github.com/%s/%s/version.txt", user, repo))
+local version = game:HttpGet(string.format("https://raw.githubusercontent.com/%s/%s/main/version.txt", user, repo))
 
 if not isfolder("gunc") then makefolder("gunc") end
 
 function download(version)
-    writefile("gunc/src.lua", game:HttpGet(string.format("https://github.com/%s/%s/%s/src.luau", user, repo, version)))
-    writefile("gunc/updater.lua", game:HttpGet(string.format("https://github.com/%s/%s/src.lua", user, repo)))
+    writefile("gunc/src.lua", game:HttpGet(string.format("https://raw.githubusercontent.com/%s/%s/%s/main/src.luau", user, repo, version)))
+    writefile("gunc/updater.lua", game:HttpGet(string.format("https://raw.githubusercontent.com/%s/%s/main/src.lua", user, repo)))
 end
 
 return {install=function() download(version) warn("gunc installed version:"..version) return loadstring(readfile("gunc/src.lua"))() end}
